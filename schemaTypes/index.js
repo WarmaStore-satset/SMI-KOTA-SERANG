92ac35357fbcddb3d0b9f17ad55c0c3d9655a526
Warmapensi <warmastore.17@gmail.com>
2026-08-08 23:12:53 +0700

    Update index.js

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,9 +1,11 @@
 import unit from './unit'
 import prestasi from './prestasi'
 import pengurus from './pengurus'
+import banner from './banner'
 
 export const schemaTypes = [
   unit,
   prestasi,
-  pengurus
+  pengurus,
+  banner
 ]
